297d290501ee4419b9ca79779eef009749dddd3a
toptalhook <toptal.cook@gmail.com>
2023-03-23 12:14:48 +0430

    install and config websocket and pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http',
                 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+//                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    broadcast(new \App\Events\WebsocketDemoEvent('some data'));
     return view('welcome');
 });
